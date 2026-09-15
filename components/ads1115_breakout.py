9717aec9ce31a9eac193afc552ab86e8b8cbeb0c
Jared Reasy <62480924+flyinthelyceum@users.noreply.github.com>
2026-09-15 11:35:11 -0700

    components: ads1115_breakout.PCB_W = 17.27 (CALIPER 2026-09-15 JR)

--- components/ads1115_breakout.py
+++ components/ads1115_breakout.py
@@ -1,3 +1,4 @@
 """ads1115_breakout: created by `components measure` on 2026-09-15. Add a description."""
 UNIT = "mm"
 PCB_L = 28.19  # CALIPER 2026-09-15 JR
+PCB_W = 17.27  # CALIPER 2026-09-15 JR
