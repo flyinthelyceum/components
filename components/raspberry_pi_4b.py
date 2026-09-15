27cba73d613f7813b7c6360e796d11ef30a72569
Jared Reasy <62480924+flyinthelyceum@users.noreply.github.com>
2026-09-15 10:27:27 -0700

    components: raspberry_pi_4b.PCB_W = 56 (DATASHEET https://pip-assets.raspberrypi.com/categories/545-raspberry-pi-4-model-b/documents/RP-008343-DS-1-raspberry-pi-4-mechanical-drawing.pdf)

--- components/raspberry_pi_4b.py
+++ components/raspberry_pi_4b.py
@@ -1,3 +1,4 @@
 """raspberry_pi_4b: created by `components measure` on 2026-09-15. Add a description."""
 UNIT = "mm"
 PCB_L = 85  # DATASHEET https://pip-assets.raspberrypi.com/categories/545-raspberry-pi-4-model-b/documents/RP-008343-DS-1-raspberry-pi-4-mechanical-drawing.pdf
+PCB_W = 56  # DATASHEET https://pip-assets.raspberrypi.com/categories/545-raspberry-pi-4-model-b/documents/RP-008343-DS-1-raspberry-pi-4-mechanical-drawing.pdf
