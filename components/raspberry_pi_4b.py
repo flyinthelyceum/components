5a981b955b501d4c99faf9b3dd85444a50f641c2
Jared Reasy <62480924+flyinthelyceum@users.noreply.github.com>
2026-09-15 10:29:05 -0700

    components: raspberry_pi_4b.HOLE_EDGE_INSET = 3.5 (DATASHEET https://pip-assets.raspberrypi.com/categories/545-raspberry-pi-4-model-b/documents/RP-008343-DS-1-raspberry-pi-4-mechanical-drawing.pdf)

--- components/raspberry_pi_4b.py
+++ components/raspberry_pi_4b.py
@@ -5,3 +5,4 @@ PCB_W = 56  # DATASHEET https://pip-assets.raspberrypi.com/categories/545-raspbe
 HOLE_DIA = 2.7  # DATASHEET https://pip-assets.raspberrypi.com/categories/545-raspberry-pi-4-model-b/documents/RP-008343-DS-1-raspberry-pi-4-mechanical-drawing.pdf
 HOLE_PITCH_X = 58  # DATASHEET https://pip-assets.raspberrypi.com/categories/545-raspberry-pi-4-model-b/documents/RP-008343-DS-1-raspberry-pi-4-mechanical-drawing.pdf
 HOLE_PITCH_Y = 49  # DATASHEET https://pip-assets.raspberrypi.com/categories/545-raspberry-pi-4-model-b/documents/RP-008343-DS-1-raspberry-pi-4-mechanical-drawing.pdf
+HOLE_EDGE_INSET = 3.5  # DATASHEET https://pip-assets.raspberrypi.com/categories/545-raspberry-pi-4-model-b/documents/RP-008343-DS-1-raspberry-pi-4-mechanical-drawing.pdf
