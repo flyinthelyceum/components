52ff98094f2a5e5a03370efa35084b4ed7efbf25
Jared Reasy <62480924+flyinthelyceum@users.noreply.github.com>
2026-09-15 11:35:31 -0700

    components: relay_module_1ch.PCB_W = 17.27 (CALIPER 2026-09-15 JR)

--- components/relay_module_1ch.py
+++ components/relay_module_1ch.py
@@ -1,3 +1,4 @@
 """relay_module_1ch: created by `components measure` on 2026-09-15. Add a description."""
 UNIT = "mm"
 PCB_L = 43.31  # CALIPER 2026-09-15 JR
+PCB_W = 17.27  # CALIPER 2026-09-15 JR
